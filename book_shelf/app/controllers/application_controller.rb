82c1d54d5630afd13ab64b3ced802061fb8d44cf
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-08 21:58:17 +0000

    book_shelf

--- book_shelf/app/controllers/application_controller.rb
+++ book_shelf/app/controllers/application_controller.rb
@@ -7,8 +7,23 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get "/" do
-    erb :welcome
-  end
+  get '/' do                  #Route - if user is able to login, thery are redirected to their personal page, if not they would 
+    if logged_in?             #be redirected back to the welcome (index) page
+      redirect 'users/index'
+    else
+      erb :index
+    end 
+  end 
 
+
+  # helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  end
 end
+
---
+++ book_shelf/app/controllers/user_controller.rb
@@ -0,0 +1,41 @@
+class UserController < ApplicationController
+  
+  get '/registrations/signup' do
+    erb :'/registrations/signup'
+  end
+  
+  post 'registrations' do
+    @user = User.new(full_name: params["full_name"], email: params["email"], password: params["password"])
+    @user.save
+    session[:user_id] = @user.id
+      puts params
+    redirect '/users/page'
+  end
+#post 'signup' - First time users, completes the forms. Once completed, the info will be saved and this we redirect them to their personal page 
+
+  get '/sessions/login' do
+    erb :'sessions/login'
+  end 
+  
+  post '/sessions' do
+    @user = User.find_by(email: params[:email], password: params[:password]) #Finds users' info
+    if @user
+      session[:user_id] = @user.id
+      puts params
+      redirect '/users/page' #if info is found, redirect them to personal page if not,
+    end
+    redirect '/sessions/login' #redirect to login page
+  end
+  
+  get '/sessions/logout' do 
+    session.clear
+    redirect '/'
+  end
+  
+  get '/users/home' do
+ @user = User.find(session[:user_id])
+  erb :'/users/home'
+  end
+end
+  
+end 
