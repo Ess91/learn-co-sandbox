f8c5d655598f7c9bb0fe5513af744f5fcb240dbe
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-10 23:07:50 +0000

    book_shelf

--- book_shelf/app/controllers/user_controller.rb
+++ book_shelf/app/controllers/user_controller.rb
@@ -5,7 +5,7 @@ class UserController < ApplicationController
   #SignUp
   get '/signup' do
     if logged_in?
-      redirect to '/users/page'     #Route - if user is able to login, thery are redirected to their personal page, if not they would
+      redirect to '/books/index'    #Route - if user is able to login, thery are redirected to their personal page, if not they would
     else
       erb :'/users/signup'   #be redirected back to the signup
     end
@@ -16,7 +16,7 @@ class UserController < ApplicationController
     @user.save
     session[:user_id] = @user.id
       puts params
-    redirect '/users/page'
+    redirect '/books/index'
   end
 #post 'signup' - First time users, completes the forms. Once completed, the info will be saved and this we redirect them to their personal page 
 
