238b1df5093edb608b7e2403884c37923802e5ed
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-12 23:16:20 +0000

    book_shelf

--- book_shelf/app/controllers/books_controller.rb
+++ book_shelf/app/controllers/books_controller.rb
@@ -11,12 +11,11 @@ class BookController < ApplicationController
   end 
   
    
-   # post '/books' do  #create action. Responds to a POST request and creates a new book based on the params from the form and saves it to the database. Once the item is created, this action redirects to the show page
- # @book = Book.create(:title => params[:title], :author => params[:author], :genre => params[:content], :price => params[:price])
-  #redirect to "/books/#{@book.id}"
-#end
-
-  end 
+    post '/books' do  #create action. Responds to a POST request and creates a new book based on the params from the form and saves it to the database. Once the item is created, this action redirects to the show page
+  @book = Book.create(:title => params[:title], :author => params[:author], :genre => params[:content], :price => params[:price])
+  redirect to "/books/#{@book.id}"
+    end
+end 
 
 #Read 
   get '/books' do 
@@ -32,29 +31,40 @@ end
 #Update
   
   get '/books/:id/edit' do  #loads the edit form in the browser 
-    @book = Book.find_by_id(params[:id])
+    @book = Book.find(params[:id])
     erb :'/books/edit_book'
   end
 
   patch '/books/:id' do #handles the edit form submission.
-  @book = Book.find_by_id(params[:id])
-  @book.title = params[:title]
-  @book.author = params[:author]
-  @book.genre = params[:genre]
-  @book.price = params[:price]
-  @book.save
-  redirect to "/books/#{@book.id}"
-end
+    @book = Book.find(params[:id])
+    @book.update(title:params[:title], author:params[:author], genre:params[:genre], price:params[:price])
+    redirect "/books/#{@book.id}"
+  end 
+  
+  #@book = Book.find_by_id(params[:id])
+  #@book.title = params[:title]
+  #@book.author = params[:author]
+  #@book.genre = params[:genre]
+  #@book.price = params[:price]
+  #@book.save
+  #redirect to "/books/#{@book.id}"
+#end
 #First, we pull the article by the ID from the URL, then we update the title and content attributes and save. The action ends with a redirect to the article show page.
 
-  #Delete 
   
   #Delete 
 
   delete '/books/:id' do #delete action
-  @book = Book.find_by_id(params[:id])
-  @book.delete
-  redirect to '/books'
-  end
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect '/books'
+  #  redirect '/articles'
+  end 
+#end 
+    
+ # @book = Book.find_by_id(params[:id])
+  #@book.delete
+  #redirect to '/books'
+  #end
  #end 
 #end 
