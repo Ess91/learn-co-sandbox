360a953ff2ac9b115eb9549b8cb2d13de943eb81
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-12 22:54:32 +0000

    book_shelf

--- book_shelf/app/controllers/books_controller.rb
+++ book_shelf/app/controllers/books_controller.rb
@@ -1,7 +1,8 @@
 class BookController < ApplicationController
   
   #Create
-  get '/books/new' do    #The first one is a GET request to load the form to create a new book.
+  
+    get '/books/new' do    #The first one is a GET request to load the form to create a new book.
     if is_logged_in?
       redirect '/books/new'
     else
@@ -9,23 +10,27 @@ class BookController < ApplicationController
    end
   end 
   
-  get 
+   
+   # post '/books' do  #create action. Responds to a POST request and creates a new book based on the params from the form and saves it to the database. Once the item is created, this action redirects to the show page
+ # @book = Book.create(:title => params[:title], :author => params[:author], :genre => params[:content], :price => params[:price])
+  #redirect to "/books/#{@book.id}"
+#end
 
+  end 
 
-  post '/books' do  #create action. Responds to a POST request and creates a new book based on the params from the form and saves it to the database. Once the item is created, this action redirects to the show page
-  @book = Book.create(:title => params[:title], :author => params[:author], :genre => params[:content], :price => params[:price])
-  redirect to "/books/#{@book.id}"
-end
-
-  #Read 
+#Read 
+  get '/books' do 
+    @books = Book.all 
+    erb :'/users/index'
+  end 
   
-
   get '/books/:id' do #order to display a single article, we need a show action. This route uses a dynamic URL, we can access the ID of the article in the view through the params hash.
   @book = Book.find_by_id(params[:id])
   erb :'/books/show'
 end
 
-  #Update
+#Update
+  
   get '/books/:id/edit' do  #loads the edit form in the browser 
     @book = Book.find_by_id(params[:id])
     erb :'/books/edit_book'
@@ -42,14 +47,14 @@ end
 end
 #First, we pull the article by the ID from the URL, then we update the title and content attributes and save. The action ends with a redirect to the article show page.
 
-#Delete 
+  #Delete 
+  
+  #Delete 
 
   delete '/books/:id' do #delete action
   @book = Book.find_by_id(params[:id])
   @book.delete
   redirect to '/books'
-end
-  
+  end
+ end 
 end 
-
- 
