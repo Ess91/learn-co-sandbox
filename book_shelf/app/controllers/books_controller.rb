beb3ab51452a973a8ea2853260a40a3c3e78051a
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-13 21:26:44 +0000

    book_shelf

--- book_shelf/app/controllers/books_controller.rb
+++ book_shelf/app/controllers/books_controller.rb
@@ -3,12 +3,12 @@ class BookController < ApplicationController
   #Create
   
     get '/books/new' do    #The first one is a GET request to load the form to create a new book.
-      @books = Book.create(params)
-      @books.save
-   # if is_logged_in?
-    #  redirect '/books/new'
-    #else
-     # redirect '/users/login'
+     # @books = Book.create(params)
+    #  @books.save
+    if is_logged_in?
+      erb:'/books/new'
+    else
+      redirect '/login'
    end
   end 
   
@@ -16,14 +16,14 @@ class BookController < ApplicationController
     post '/books' do  #create action. Responds to a POST request and creates a new book based on the params from the form and saves it to the database. Once the item is created, this action redirects to the show page
  # @book = Book.create(:title => params[:title], :author => params[:author], :genre => params[:content], :price => params[:price])
   @books = Book.create(params)
-  redirect to "/books/#{@book.id}"
+  redirect "/books/#{@book.id}"
     end
 #end 
 
 #Read 
   get '/books' do 
     @books = Book.all 
-    erb :'/books/index'
+    redirect '/books'
   end 
   
   get '/books/:id' do #order to display a single article, we need a show action. This route uses a dynamic URL, we can access the ID of the article in the view through the params hash.
@@ -45,7 +45,7 @@ end
     @book.genre = params[:genre]
     @book.price = params[:price]
     @book.save
-  redirect to "/books/#{@book.id}"
+  redirect "/books/#{@book.id}"
 end
 #First, we pull the article by the ID from the URL, then we update the title and content attributes and save. The action ends with a redirect to the article show page.
 
@@ -58,5 +58,5 @@ end
     redirect '/books'
   
   end 
-#end 
+end 
     
