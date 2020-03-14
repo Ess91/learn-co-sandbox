27435985d78316034c0d459bc351743a23d8a2fb
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-14 00:21:53 +0000

    book_shelf

--- book_shelf/app/controllers/user_controller.rb
+++ book_shelf/app/controllers/user_controller.rb
@@ -6,7 +6,7 @@ class UserController < ApplicationController
   
   get '/signup' do
   #  if logged_in?
-   #   redirect '/books/index'    #Route - if user is able to login, thery are redirected to their personal page, if not they would
+   #   redirect '/books'    #Route - if user is able to login, thery are redirected to their personal page, if not they would
   #  else
       erb :'/users/signup'   #be redirected back to the signup
     end
@@ -14,17 +14,29 @@ class UserController < ApplicationController
     
     
     post '/signup' do 
-     if params[:username] != "" && params[:email] != "" && params[:password] != "" 
+      if logged_in?
+        redirect '/books'
+     elsif params[:username] != "" && params[:email] != "" && params[:password] != "" 
      @new_user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
      @new_user.save
      session[:user_id] = @new_user.id
      redirect '/books/index'
     else
-      erb :'/users/signup'
+      redirect '/users/signup'
     end
-  end
-      
+  #end
   
+
+      
+ get '/users/index' do
+   if logged_in?
+    erb :'/users/index'
+  else
+    redirect '/login'
+  end 
+ #end 
+
+ 
  
   #Login
  get '/login' do
@@ -52,5 +64,6 @@ end
     session.clear
     redirect '/'
   end
-# end 
+end 
+end 
 
