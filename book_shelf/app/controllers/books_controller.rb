b204a9286fe32918a8de95a156541e0cb7cb1fba
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-16 23:12:35 +0000

    book_shelf

--- book_shelf/app/controllers/books_controller.rb
+++ book_shelf/app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BookController < ApplicationController
  #   end
   #create action. Responds to a POST request and creates a new book based on the params from the form and saves it to the database. Once the item is created, this action redirects to the show page
   
-  post '/reviews' do
+  post '/books' do
     if params[:title] == "" || params[:author] == "" || params[:genre] == "" || params[:price] == "" # must have title, genre, content, & rating
       
       redirect to '/books/new'
@@ -36,10 +36,10 @@ class BookController < ApplicationController
 
 
 #READ
-  get '/books' do 
-    @books = Book.all
-    erb :index
-  end 
+ # get '/books' do 
+  #  @books = Book.all
+  #  erb :index
+  #end 
    # if logged_in?
     #  @user = current_user
      # @books = @user.books.all 
@@ -51,17 +51,50 @@ class BookController < ApplicationController
 #  end 
 #end 
 
-  get '/books/:id' do 
-  @book = Book.find_by_id(params[:id])
-  erb :'/books/show'
-end
+get '/books' do
+    if logged_in?
+      @user = current_user
+      @books = Book.all
+      erb :'books/index'
+    else
+      redirect to '/users/login'
+    end
+  end
+  
+  
+ # get '/books/:id' do 
+#  @books = Book.find_by_id(params[:id])
+#  erb :'/books/show'
+#end
 #Order to display a single article, we need a show action. This route uses a dynamic URL, we can access the ID of the article in the view through the params hash.
+
+  get '/books/:id' do
+    if logged_in?
+      @book = Book.find_by_id(params[:id])
+      erb :'books/show'
+    else
+      redirect to '/login'
+    end
+  end
 
 #UPDATE
   
-  get '/books/:id/edit' do  #loads the edit form in the browser 
-    @book = Book.find(params[:id])
-    erb :'/books/edit'
+ # get '/books/:id/edit' do  #loads the edit form in the browser 
+  #  @book = Book.find(params[:id])
+  #  erb :'/books/edit'
+  #end
+  
+   get '/books/:id/edit' do
+    if logged_in?
+      @book = Book.find_by_id(params[:id])
+      if @book && @book.user == current_user
+        erb :'books/edit'
+      else
+        redirect to '/books'
+      end
+    else
+      redirect to '/login'
+    end
   end
 
   patch '/books/:id' do #handles the edit form submission.
@@ -74,6 +107,9 @@ end
   redirect "/books/#{@book.id}"
 end
 #First, we pull the article by the ID from the URL, then we update the title and content attributes and save. The action ends with a redirect to the article show page.
+
+
+  
 
   
   #Delete 
