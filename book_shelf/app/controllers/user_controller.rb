90173f2518f7c8871fe50935b450a0274b6149f9
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-17 23:11:44 +0000

    book_shelf

--- book_shelf/app/controllers/user_controller.rb
+++ book_shelf/app/controllers/user_controller.rb
@@ -23,6 +23,21 @@ class UserController < ApplicationController
     end
   end
 
+  #LOGIN
   
+ get '/login' do
+    erb :'/users/login'
+end 
+
+  post '/login' do
+    @current_user = User.find_by(:username => params[:username])
+    
+     if @current_user && @current_user.authenticate(params[:password])
+    session[:user_id] = @current_user.id
+    redirect '/books'
+  else
+    redirect '/users/login'
+  end
+ end
 end 
 
