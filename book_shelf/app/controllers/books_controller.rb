eaec9572d8b3bc09942cfcc39946035387eb3f91
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-17 23:40:02 +0000

    book_shelf

--- book_shelf/app/controllers/books_controller.rb
+++ book_shelf/app/controllers/books_controller.rb
@@ -1,3 +1,21 @@
 class BookController < ApplicationController
   
+   #Create
+  
+    get '/books' do
+"hello"
+end 
+
+
+  
+    get '/books/new' do    #The first one is a GET request to load the form to create a new book.
+    if logged_in?
+      erb :'/books/new'
+    else
+      redirect '/users/login'
+   end
+  end 
+
+
+  
 end 
--- book_shelf/app/controllers/user_controller.rb
+++ book_shelf/app/controllers/user_controller.rb
@@ -3,12 +3,8 @@ require './config/environment'
 class UserController < ApplicationController
   
   get '/signup' do 
-    if logged_in?
-         redirect to '/books'
-    else
       erb :'/users/signup'
     end
-  end
   
   post '/signup' do 
       if logged_in?
