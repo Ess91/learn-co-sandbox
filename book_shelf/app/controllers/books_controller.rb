892214d84fe164fc0e6c978391151b7fe184275e
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-17 23:51:12 +0000

    book_shelf

--- book_shelf/app/controllers/books_controller.rb
+++ book_shelf/app/controllers/books_controller.rb
@@ -1,13 +1,19 @@
 class BookController < ApplicationController
   
-   #Create
-  
+   
+  #READ
     get '/books' do
-"hello"
+    if logged_in?
+      @user = current_user 
+      @books = Book.all 
+      erb :'/books/index'
+    else
+      redirect '/login'
+    end 
 end 
 
 
-  
+  #CREATE
     get '/books/new' do    #The first one is a GET request to load the form to create a new book.
     if logged_in?
       erb :'/books/new'
@@ -16,6 +22,12 @@ end
    end
   end 
 
+   post '/books' do  
+  @book = Book.create(:title => params[:title], :author => params[:author], :genre => params[:content], :price => params[:price])
+  @books = Book.create(params)
+  redirect "/books/#{@book.id}"
+    end
+  #create action. Responds to a POST request and creates a new book based on the params from the form and saves it to the database. Once the item is created, this action redirects to the show page
 
   
 end 
