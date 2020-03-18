c18f04e902e234d4e5fa9fc862149ad8810963ed
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-18 21:49:34 +0000

    book_shelf

--- book_shelf/app/controllers/books_controller.rb
+++ book_shelf/app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BookController < ApplicationController
   #READ
     get '/books' do
     if logged_in?
-      @user = current_user 
+    #  @user = current_user 
       @books = Book.all 
       erb :'/books/index'
     else
@@ -21,23 +21,21 @@ end
       redirect '/users/login'
    end
   end 
+  
 
-  # post '/books' do  
-#  @book = Book.create(:title => params[:title], :author => params[:author], :genre => params[:content], :price => params[:price])
- # @books = Book.create(params)
-#  redirect "/books/#{@book.id}"
- #   end
+  post '/books' do  
+  @book = Book.create(:title => params[:title], :author => params[:author], :genre => params[:genre], :price => params[:price])
+  redirect "/books/#{@book.id}"
+end
  
-    post '/books' do 
-       if logged_in? && params[:title] != "" && params[:author] != "" && params[:genre] != "" && params[:price] != ""
-         @new_book = Book.create(params)
-         @new_book.user_id = current_user.id 
-         @new_book.save 
-         redirect '/users/index'
-       else
-         redirect '/users/index'
-       end 
-     end 
+ #   post '/books' do 
+  #     if logged_in? && params[:title] != "" && params[:author] != "" && params[:genre] != "" && params[:price] != ""
+   #      @new_book = Book.create(params)
+    #     @new_book.user_id = current_user.id 
+     #    @new_book.save 
+    #     redirect '/users/index'
+     #  end 
+     #end 
          
          
   #create action. Responds to a POST request and creates a new book based on the params from the form and saves it to the database. Once the item is created, this action redirects to the show page
