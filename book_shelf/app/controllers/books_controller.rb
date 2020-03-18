a4e15a89765f0fbe0a4213f8c8730049b0c89971
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-18 22:33:08 +0000

    book_shelf

--- book_shelf/app/controllers/books_controller.rb
+++ book_shelf/app/controllers/books_controller.rb
@@ -36,7 +36,46 @@ end
     #     redirect '/users/index'
      #  end 
      #end 
+     
+       get '/books/:id' do 
+  if logged_in?
+    @book = Book.find_by_id(params[:id])
+    erb :'books/new'
+  else 
+    redirect 'users/login'
+  end 
+end
+
+  get '/books/:id' do
+    @book = Book.find_by_id(params[:id])
+    erb :show
+  end
+  
+  get '/books/:id/edit' do
+    @book = Book.find_by_id(params[:id])
+    erb :'/books/edit'
+  end
+  
+  patch '/books/:id' do #handles the edit form submission.
+    @book = Book.find_by_id(params[:id])
+    @book.title = params[:title]
+    @book.author = params[:author]
+    @book.genre = params[:genre]
+    @book.price = params[:price]
+    @book.save
+  redirect "/books/#{@book.id}"
+end
+
+#First, we pull the article by the ID from the URL, then we update the title and content attributes and save. The action ends with a redirect to the article show page.
+
+  delete '/books/:id' do
+    Recipe.destroy(params[:id])
+    redirect to '/books'
+  end
+
+end	
+
          
          
   #create action. Responds to a POST request and creates a new book based on the params from the form and saves it to the database. Once the item is created, this action redirects to the show page
-end 
+#end 
