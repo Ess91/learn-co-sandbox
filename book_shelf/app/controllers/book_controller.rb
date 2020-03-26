9f1f30b1523b7117b15b36c999a6766a56e161e1
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-26 22:39:59 +0000

    book_shelf

--- book_shelf/app/controllers/book_controller.rb
+++ book_shelf/app/controllers/book_controller.rb
@@ -66,7 +66,7 @@ end
       redirect "/books/#{@book.id}/edit"
   end
 end 
-#  else 
+  else 
 redirect 'users/login'
 end 
 
@@ -78,7 +78,7 @@ end
     @book.destroy
     redirect '/books'
   else #this else belongs to line 77
-#  else #this else belongs to the logged_in?
+  else #this else belongs to the logged_in?
     redirect 'users/login'
   end
  end 
