969e3affa7821b945ab6a4e29c1d2a24bab26dd2
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-03-26 22:26:51 +0000

    book_shelf

--- book_shelf/app/models/user.rb
+++ book_shelf/app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
+  
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  
   has_secure_password
   has_many :book
   
-
 end
 
 #Validations mean a User object won't ever be saved unless they meet this criteria. The above code says that a User cannot exist in the database unless it has
