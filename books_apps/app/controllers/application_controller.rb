5b90b2187681bcc3be93e69050b5dddcc97ca5f4
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-04-20 21:32:02 +0000

    books_apps

--- books_apps/app/controllers/application_controller.rb
+++ books_apps/app/controllers/application_controller.rb
@@ -2,13 +2,24 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  # enable sessions
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'book_reader'
+  end 
+  
+   # Homepage
+  get '/' do
+    erb :index
   end
 
-  get "/" do
-    erb :welcome
+  def is_logged_in?
+    !!session[:user_id]
   end
 
+  def current_user
+    @user = User.find(session[:user_id])
+  end
 end
