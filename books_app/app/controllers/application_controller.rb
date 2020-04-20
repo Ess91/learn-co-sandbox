4637a455922abd99020c4a1ea112d1662c095b42
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-04-20 20:40:07 +0000

    books_app

--- books_app/app/controllers/application_controller.rb
+++ books_app/app/controllers/application_controller.rb
@@ -5,10 +5,27 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "bookreader"
   end
 
-  get "/" do
-    erb :welcome
+ #HOMEPAGE
+  get '/' do 
+    erb :index
+  end 
+
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+   @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-end
+  get '/books/index' do #may not need this code but getting error to say I need it
+   # "hello"
+    erb :'/books/index'
+  end
+end 
+
